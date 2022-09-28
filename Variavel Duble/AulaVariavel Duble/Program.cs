004b0e70693f8a74810485ee9336fd9631100cc9
kallyane-nascimento16 <110573013+kallyane-nascimento16@users.noreply.github.com>
2022-09-28 18:36:19 -0300

    Add files via upload

---
+++ Variavel Duble/AulaVariavel Duble/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Aula2
+{
+    public static void Main(string[] args)
+
+    {
+
+        Double Altura = 1.60;
+        Double Peso = 70.49;
+        Double divisao = 5.10 / 2;
+        Double multiplicao = 12.0 * 15.0;
+        Double subtracao = 25.0 - 32.0;
+
+        Console.WriteLine(" a divisão é " + divisao);
+
+
+
+    }
+
+
+
+}
+
+
+
+
---
+++ Variavel Duble/int/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class aula2
+{
+    public static void Main(String[]args)
+        {
+
+        int numero;
+
+        Console.WriteLine("digite um numero ");
+         numero=int.Parse(Console.ReadLine());
+
+        Console.WriteLine("o numero que vc digitou " + numero);
+
+
+
+
+    }
+}
